1e303abd88f57b56c25ce45511f0a4bf5ddfd84a
kirjavascript <snkenjoi@gmail.com>
2020-02-25 22:58:47 +0000

    committed the js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import Cube from './cube';
  * examples
  *
  * stickerless
+ * backface
  * japanese
+ * snap to position
  * alg demo
  * solve playback
  * timer
@@ -42,19 +44,20 @@ function zsim(container) {
 
     const cube = Cube({ illo, zoom, colors, cubeColor });
     console.log(cube);
+    // move queue
 
     // const { anchor: anc } = ref;
 
     (function loop() {
-        [5, 9, 4, 1].map(i => cube.edges[i]).forEach(({ anchor }) => {
-            anchor.rotate.x += 0.05;
-        });
+        // [5, 9, 4, 1].map(i => cube.edges[i]).forEach(({ anchor }) => {
+        //     anchor.rotate.x += 0.05;
+        // });
 
-        [5, 4, 0, 1].map(i => cube.corners[i]).forEach(({ anchor }) => {
-            anchor.rotate.x += 0.05;
-        })
+        // [5, 4, 0, 1].map(i => cube.corners[i]).forEach(({ anchor }) => {
+        //     anchor.rotate.x += 0.05;
+        // })
 
-        cube.centres[2].anchor.rotate.x += 0.05;
+        // cube.centres[2].anchor.rotate.x += 0.05;
 
         // if (anc.rotate.z < TAU / 4) {
         //     anc.rotate.z += 0.05;
